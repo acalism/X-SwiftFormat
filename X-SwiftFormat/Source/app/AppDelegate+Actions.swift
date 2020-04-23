68554bb7059bb6b8373a823c5ca2c6766c73f73c
Rui Aureliano <ruiaureliano@gmail.com>
2020-04-23 10:58:08 +0100

    Version 1.0
    + Remove update check
    + Add AppCenter

--- X-SwiftFormat/Source/app/AppDelegate+Actions.swift
+++ X-SwiftFormat/Source/app/AppDelegate+Actions.swift
@@ -10,48 +10,6 @@ import Cocoa
 
 extension AppDelegate {
 
-	@IBAction func appVersionButtonPopover(_ button: NSButton) {
-		let width = 250
-		let controller = NSViewController()
-		let popover = NSPopover()
-		self.popover = popover
-		controller.view = NSView(frame: NSRect(x: 0, y: 0, width: width, height: 80))
-		popover.contentViewController = controller
-		popover.contentSize = controller.view.frame.size
-		popover.behavior = .semitransient
-		popover.animates = true
-
-		let textField = NSTextField(string: "🚀 New Version: \(appVersionName ?? "Version 1")")
-		textField.frame = NSRect(x: 10, y: 42, width: width - 20, height: 20)
-		textField.isBezeled = false
-		textField.drawsBackground = true
-		textField.backgroundColor = .clear
-		textField.isEditable = false
-		textField.isSelectable = false
-		textField.alignment = .center
-		controller.view.addSubview(textField)
-
-		let downloadVersion = NSButton(frame: NSRect(x: (width - 100) / 2, y: 5, width: 100, height: 32))
-		downloadVersion.isBordered = false
-		downloadVersion.title = "Download"
-		downloadVersion.image = NSImage(named: "default-download-bt")
-		downloadVersion.target = self
-		downloadVersion.action = #selector(self.appVersionButtonPress(_:))
-		downloadVersion.bezelStyle = .rounded
-		controller.view.addSubview(downloadVersion)
-
-		popover.show(relativeTo: .zero, of: button, preferredEdge: .maxY)
-	}
-
-	@objc func appVersionButtonPress(_ button: NSButton) {
-		popover?.close()
-		if let appVersionButtonURL = appVersionButtonURL {
-			if let url = URL(string: appVersionButtonURL) {
-				NSWorkspace.shared.open(url)
-			}
-		}
-	}
-
 	@IBAction func andreTwitterPress(_ button: AnimatedButton) {
 		if let url = URL(string: "https://twitter.com/andre_goncalves") {
 			NSWorkspace.shared.open(url)
--- X-SwiftFormat/Source/app/AppDelegate.swift
+++ X-SwiftFormat/Source/app/AppDelegate.swift
@@ -6,7 +6,12 @@
 //  Copyright © 2020 Rui Aureliano. All rights reserved.
 //
 
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 import Cocoa
+
+let kAppCenterID = "05ea6538-430a-4b3d-b511-c9dcb8ca270a"
 
 @NSApplicationMain class AppDelegate: NSObject, NSApplicationDelegate {
 
@@ -16,7 +21,6 @@ import Cocoa
 	@IBOutlet weak var appIcon: NSImageView!
 	@IBOutlet weak var appName: NSTextField!
 	@IBOutlet weak var appVersion: NSTextField!
-	@IBOutlet weak var appVersionButton: NSButton!
 	var appVersionName: String?
 	var appVersionButtonURL: String?
 
@@ -32,6 +36,13 @@ import Cocoa
 	private var effectiveAppearanceObservation: NSKeyValueObservation?
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
+
+		MSAppCenter.start(kAppCenterID, withServices: [MSAnalytics.self, MSCrashes.self])
+		MSAppCenter.setLogLevel(.none)
+		if let countryCode = Locale.current.regionCode {
+			MSAppCenter.setCountryCode(countryCode)
+		}
+		MSCrashes.setEnabled(true)
 
 		changeApplicationIconImage()
 
@@ -70,7 +81,6 @@ import Cocoa
 
 	func applicationDidBecomeActive(_ notification: Notification) {
 		changeApplicationIconImage()
-		checkForNewVersion()
 	}
 
 	func applicationDidResignActive(_ notification: Notification) {
@@ -97,54 +107,6 @@ import Cocoa
 				if let icon = AppIcon.with(image: iconOverlayCopy, with: .selectedContentBackgroundColor) {
 					NSApp.applicationIconImage = icon
 				}
-			}
-		}
-	}
-
-	func checkForNewVersion() {
-		if let url = URL(string: kXSwiftFormatReleases) {
-			let request = URLRequest(url: url)
-			let task = URLSession.shared.dataTask(with: request) { data, _, _ in
-				if let data = data {
-					if let releases = data.json as? [[String: Any]] {
-						if let release = releases.first {
-							if let name = release["name"] as? String,
-								let tagName = release["tag_name"] as? String,
-								let publishedAt = release["published_at"] as? String,
-								let assets = release["assets"] as? [[String: Any]] {
-								if let asset = assets.first {
-									if let browserDownloadURL = asset["browser_download_url"] as? String {
-										self.validateNewVersion(
-											name: name,
-											tagName: tagName,
-											publishedAt: publishedAt,
-											browserDownloadURL: browserDownloadURL
-										)
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			task.resume()
-		}
-	}
-
-	private func validateNewVersion(name: String, tagName: String, publishedAt: String, browserDownloadURL: String) {
-		if let version = Bundle.main.CFBundleVersion {
-			switch version.compare(tagName, options: .numeric, range: nil, locale: nil) {
-			case .orderedAscending:
-				DispatchQueue.main.async {
-					self.appVersionName = name
-					self.appVersionButtonURL = browserDownloadURL
-					self.appVersionButton.isEnabled = true
-					self.appVersionButton.isHidden = false
-				}
-			case .orderedSame:
-				break
-			case .orderedDescending:
-				break
 			}
 		}
 	}
